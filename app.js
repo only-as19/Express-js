7cb0c24e325f0184690f2b0d9b7991355db39539
Asad <hellohello51981@gmail.com>
2026-01-12 22:42:11 +0500

    feat: Write Header

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const http = require('http')
 
 const server = http.createServer((req,res)=>{
-    console.log('user hit the server')
-    res.end('Home Page')
+    // write header
+    res.writeHead(404,{"content-type": "text/html"})
+    res.write('<h1>Home Page</h1>')
+    res.end()
 })
 
 server.listen(5000)
