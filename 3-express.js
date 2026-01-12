7ae2ca2f23a4810c4c476cedda9b5f7e1b4f354c
Asad <hellohello51981@gmail.com>
2026-01-12 23:23:32 +0500

    feat: express js info

---
+++ 3-express.js
@@ -0,0 +1,6 @@
+// Express: Express is a minimal, flexible, node js web app framework 
+// designed to make developing websites, web apps and apps much faster and easier. 
+
+// not built in
+
+// npm install express --save
