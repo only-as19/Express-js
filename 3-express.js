5dbbd2a10c7aee26a27d6dbe916aa1588738bdaf
Asad <hellohello51981@gmail.com>
2026-01-13 00:11:22 +0500

    feat: express Basics

--- 3-express.js
+++ 3-express.js
@@ -4,3 +4,24 @@
 // not built in
 
 // npm install express --save
+
+const express = require('express')
+
+const app = express()
+
+app.get('/',(req,res)=>{
+    console.log('user hit the server')
+})
+
+app.listen(5000)
+
+// methods
+
+// app.get : read file
+// app.post : insert data
+// app.put : update data
+// app.delete : delete data
+// app.all : works with every one
+// app.listen : same as http.listen
+// app.use
+
--- app.js
+++ app.js
@@ -1,52 +1,36 @@
-const http = require('http')
-const { readFileSync } = require('fs')
+// Express: Express is a minimal, flexible, node js web app framework 
+// designed to make developing websites, web apps and apps much faster and easier. 
 
-// get all files
-const homePage = readFileSync('./navbar-app/index.html')
-const homeStyles = readFileSync('./navbar-app/styles.css')
-const homeImage = readFileSync('./navbar-app/logo.svg')
-const homeLogic = readFileSync('./navbar-app/browser-app.js')
+// not built in
 
-const server = http.createServer((req, res) => {
-  // console.log(req.method)
-  const url = req.url
-  console.log(url)
-  // home page
-  if (url === '/') {
-    res.writeHead(200, { 'content-type': 'text/html' })
-    res.write(homePage)
-    res.end()
-  }
-  // about page
-  else if (url === '/about') {
-    res.writeHead(200, { 'content-type': 'text/html' })
-    res.write('<h1>about page</h1>')
-    res.end()
-  }
-  // styles
-  else if (url === '/styles.css') {
-    res.writeHead(200, { 'content-type': 'text/css' })
-    res.write(homeStyles)
-    res.end()
-  }
-  // image/logo
-  else if (url === '/logo.svg') {
-    res.writeHead(200, { 'content-type': 'image/svg+xml' })
-    res.write(homeImage)
-    res.end()
-  }
-  // logic
-  else if (url === '/browser-app.js') {
-    res.writeHead(200, { 'content-type': 'text/javascript' })
-    res.write(homeLogic)
-    res.end()
-  }
-  // 404
-  else {
-    res.writeHead(404, { 'content-type': 'text/html' })
-    res.write('<h1>page not found</h1>')
-    res.end()
-  }
+// npm install express --save
+
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  console.log('user hit the resource')
+  res.status(200).send('Home Page')
 })
 
-server.listen(5000)
+app.get('/about', (req, res) => {
+  res.status(200).send('About Page')
+})
+
+app.all('*', (req, res) => {
+  res.status(404).send('<h1>resource not found</h1>')
+})
+
+app.listen(5000)
+
+// methods
+
+// app.get : read file
+// app.post : insert data
+// app.put : update data
+// app.delete : delete data
+// app.all : works with every one
+// app.listen : same as http.listen
+// app.use
+
