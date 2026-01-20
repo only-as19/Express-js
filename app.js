5d7088d90969124c5556ed03a9cbe9b0727443c4
Asad <hellohello51981@gmail.com>
2026-01-20 22:56:38 +0500

    feat: HTTP-Methods: Delete method

--- app.js
+++ app.js
@@ -24,6 +24,16 @@ app.post('/login',(req,res)=>{
     res.status(401).send("Please enter valid credentials")
 })
 
+app.post('/api/postman/people', (req, res) => {
+  const { name } = req.body
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: 'please provide name value' })
+  }
+  res.status(201).json({ success: true, data: [...people, name] })
+})
+
 app.post('/api/people', (req, res) => {
   const { name } = req.body
   if (!name) {
@@ -34,13 +44,49 @@ app.post('/api/people', (req, res) => {
   res.status(201).json({ success: true, person: name })
 })
 
+// PUT
+
+app.put('/api/people/:id',(req,res)=>{
+    const {name} = req.body
+    const {id} = req.params
+
+    const person = people.find(name=> name.id === Number(id))
+
+    if(!person){
+        
+        return res.status(404).json({success: false,msg:`No one found with id ${id}`})
+    }
+
+    const newPeople = people.map((person)=>{
+        if(person.id === Number(id)){
+            person.name = name
+        }
+        return person
+    })
+    res.status(200).json({success:true,data: newPeople})
+})
+
+// DELETE
+
+app.delete('/api/people/:id',(req,res)=>{
+    const {id} = req.params
+
+    const person = people.find(name=> name.id === Number(id))
+
+    if(!person){
+        return res.status(404).json({success: false,msg:`No one found with id ${id}`})
+    }
+
+    const newPeople = people.filter((person)=>{
+        return person.id !== Number(id)
+    })
+    return res.status(200).json({success:true,data: newPeople})
+})
 
 app.listen(5000,()=>{
     console.log('server is listening');
     
 })
 
-// PUT
-// DELETE
 
 
